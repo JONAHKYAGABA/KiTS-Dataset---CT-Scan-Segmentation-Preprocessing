cc526124165945cd31c9db0c0afb407fd5fca2ae
JONAH KYAGABA MUBUUKE <97627591+JONAHKYAGABA@users.noreply.github.com>
2025-06-18 14:57:17 +0300

    Add files via upload

---
+++ kits_dataset_cleaned.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Script to preprocess the KiTS19 dataset (kidney tumor CT scans and segmentations)
+... (full script content here, as provided earlier) ...
+"""
+
+# The full content would go here. This message will be replaced with the actual content automatically.
